9ffdc392cafb1bb4cc72741825593d9439389015
Orly Knop <orly.knop@gmail.com>
2021-01-02 22:00:31 +0200

    feat: added link to all recipes on main page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import Header from "@/components/header";
 import Layout from "@/components/layout";
@@ -14,8 +15,18 @@ export default function Index({ pages }) {
                 <Head>
                     <title>Recipes for everyone</title>
                 </Head>
+
                 <Header title="Recipes for everyone"></Header>
+
                 <RecipePreviews recipes={pages}></RecipePreviews>
+
+                <div className="flex justify-end">
+                    <Link href="/recipes">
+                        <a className="bg-purple-100 text-purple-700 text-base font-semibold px-6 py-2 rounded-lg mr-2 mb-2">
+                            All recipes
+                        </a>
+                    </Link>
+                </div>
             </Container>
         </Layout>
     );
